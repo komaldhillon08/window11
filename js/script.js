4df0b5a77d0fa9f6e26df1218cd9a55de25d6e40
Sandeep <your-email@example.com>
2025-03-25 10:56:35 +0530

    add new project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("komal");
+
+let taskbar = document.querySelector(".taskbar"); 
+let startmanu = document.querySelector(".startmanu");
+
+
+taskbar.addEventListener( "click" , function()  {
+    console.log("komal");
+    
+    if (startmanu.style.bottom == "50px") {
+        startmanu.style.bottom = "-650px"
+    }else{
+        startmanu.style.bottom = "50px"
+    }
+} ) ;
+
